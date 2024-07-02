76cd53d9bcc2ff7904b53f68caef1d75b1442f28
sitaulasuman303 <sitaulasuman303@gmail.com>
2024-07-02 22:41:43 +0545

    bug fix

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
-import React from "react";
+import React, { useMemo, useRef } from "react";
 import { SubTrigger } from "zeego/context-menu";
 import RoundButton from "@/components/RoundButton";
 import { Ionicons } from "@expo/vector-icons";
@@ -9,6 +9,7 @@ import { defaultStyles } from "@/constants/Styles";
 import Colors from "@/constants/Colors";
 import WidgetList from "@/components/SortableList/WidgetList";
 import { useHeaderHeight } from "@react-navigation/elements";
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const HomeScreen = () => {
   const { balance, runTransaction, transaction, clearTransaction } =
@@ -22,74 +23,104 @@ const HomeScreen = () => {
       title: "Money added",
     });
   };
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
+
+  // handler to open bottom sheet
+  const handleOpenBottomSheet = () => {
+    bottomSheetRef.current?.expand();
+  };
+
+  // handler to close bottom sheet
+  const handleCloseBottomSheet = () => {
+    bottomSheetRef.current?.close();
+  };
   const reversedTransaction = [...transaction].reverse();
   return (
-    <ScrollView
-      style={{ backgroundColor: Colors.background }}
-      contentContainerStyle={{ paddingTop: headerHeight }}
-    >
-      <View style={styles.account}>
-        <View style={styles.row}>
-          <Text style={styles.balance}>{balance()}</Text>
-          <Text style={styles.currency}>$</Text>
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        style={{ backgroundColor: Colors.background }}
+        contentContainerStyle={{ paddingTop: headerHeight }}
+      >
+        <View style={styles.account}>
+          <View style={styles.row}>
+            <Text style={styles.balance}>{balance()}</Text>
+            <Text style={styles.currency}>$</Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.actionRow}>
-        <RoundButton text="Add money" icon={"add"} onClick={addMoney} />
-        <RoundButton text="Exchange" icon={"refresh"} onClick={addMoney} />
-        <RoundButton text="Details" icon={"list"} onClick={addMoney} />
-        <Dropdown />
-      </View>
-      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
-      <View style={styles.transactions}>
-        {transaction.length === 0 ? (
-          <Text>No transcations performed</Text>
-        ) : (
-          reversedTransaction.map((transaction) => {
-            const date = transaction.date + "a";
-            const newDate = date.slice(0, 9);
-            const newTime = date.slice(11, 18);
-            return (
-              <View
-                key={transaction.id}
-                style={{
-                  flexDirection: "row",
-                  padding: 10,
-                  alignItems: "center",
-                  gap: 5,
-                }}
-              >
-                <View style={styles.circle}>
-                  <Ionicons
-                    name={transaction.amount > 0 ? "add" : "remove"}
-                    size={24}
-                    color={Colors.dark}
-                  />
+        <View style={styles.actionRow}>
+          <RoundButton
+            text="Add money"
+            icon={"add"}
+            onClick={handleOpenBottomSheet}
+          />
+          <RoundButton text="Exchange" icon={"refresh"} onClick={addMoney} />
+          <RoundButton text="Details" icon={"list"} onClick={addMoney} />
+          <Dropdown />
+        </View>
+        <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+        <View style={styles.transactions}>
+          {transaction.length === 0 ? (
+            <Text>No transcations performed</Text>
+          ) : (
+            reversedTransaction.map((transaction) => {
+              const date = transaction.date + "a";
+              const newDate = date.slice(0, 9);
+              const newTime = date.slice(11, 18);
+              return (
+                <View
+                  key={transaction.id}
+                  style={{
+                    flexDirection: "row",
+                    padding: 10,
+                    alignItems: "center",
+                    gap: 5,
+                  }}
+                >
+                  <View style={styles.circle}>
+                    <Ionicons
+                      name={transaction.amount > 0 ? "add" : "remove"}
+                      size={24}
+                      color={Colors.dark}
+                    />
+                  </View>
+                  <View>
+                    <Text>{transaction.title}</Text>
+                    <Text>{newDate + ", " + newTime}</Text>
+                  </View>
+                  <View style={{ flex: 1 }} />
+                  <View style={styles.details}>
+                    <Text
+                      style={
+                        transaction.amount > 0
+                          ? { color: "green" }
+                          : { color: "red" }
+                      }
+                    >
+                      {transaction.amount}
+                    </Text>
+                  </View>
                 </View>
-                <View>
-                  <Text>{transaction.title}</Text>
-                  <Text>{newDate + ", " + newTime}</Text>
-                </View>
-                <View style={{ flex: 1 }} />
-                <View style={styles.details}>
-                  <Text
-                    style={
-                      transaction.amount > 0
-                        ? { color: "green" }
-                        : { color: "red" }
-                    }
-                  >
-                    {transaction.amount}
-                  </Text>
-                </View>
-              </View>
-            );
-          })
-        )}
-      </View>
+              );
+            })
+          )}
+        </View>
 
-      <WidgetList />
-    </ScrollView>
+        <WidgetList />
+      </ScrollView>
+      <BottomSheet snapPoints={snapPoints} ref={bottomSheetRef} index={-1}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            borderRadius: 40,
+            padding: 16,
+          }}
+        >
+          <Text>hello</Text>
+        </View>
+      </BottomSheet>
+    </View>
   );
 };
 const styles = StyleSheet.create({
